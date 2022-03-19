d674259274597c2014b1b44bcc2fc6c2eed88516
Cesar Rozaboni <cesarrozaboni@hotmail.com>
2022-03-19 19:49:01 -0300

    Add project files.

---
+++ SignalR_Chat/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR_Chat
+{
+    public class ChatHub : Hub
+    {
+        public async Task Message_To_Server(string user, string message)
+        {
+            await Clients.All.SendAsync("Message_To_Client", user, message);
+        }
+    }
+}
---
+++ SignalR_Chat/wwwroot/js/chat.js
@@ -0,0 +1,89 @@
+﻿'use strict';
+
+//create conexion with signalR
+const connection = CreateConnection();
+
+//function that receive messages from server hub
+GetMessageServer();
+//function to send message to server hub
+SetMessageServer();
+//Start conection with server
+StartConnection();
+
+SetKeyDown();
+
+//#region Create connexion with server
+function CreateConnection() {
+    return new signalR.HubConnectionBuilder().withUrl('/chatHub').build();
+}
+//#endregion
+
+//#region Receive messages from server hub
+function GetMessageServer() {
+    connection.on('Message_To_Client', function (user, message) {
+        var msg = message.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+        var encodeMsg = `${user} Diz: ${msg} \n`;
+
+        $('#messageList').append(encodeMsg);
+
+        if ($('#messageList').length)
+            $('#messageList').scrollTop($('#messageList')[0].scrollHeight - $('#messageList').height());
+    });
+}
+//#endregion
+
+//#region Send message to server hub
+function SetMessageServer() {
+    $('#btnMessage').click(function (event) {
+        var user = $('#txtUser').val();
+        var message = $('#txtMessage').val();
+
+        if (!ValueIsValid())
+            return;
+
+        connection.invoke('Message_To_Server', user, message).catch(function (err) {
+            return console.error(err.toString());
+        });
+
+        event.preventDefault();
+        $('#txtMessage').val('');
+        $('#txtMessage').focus();
+    });
+}
+
+function ValueIsValid() {
+    if ($('#txtUser').val() == '') {
+        alert('Usuario não informado!');
+        return false;
+    }
+
+    if ($('#txtMessage').val() == '') {
+        alert('Mensagem não pode ser vazia!');
+        return false;
+    }
+    $('#txtUser').prop('disabled', 'disabled');
+
+    return true;
+}
+
+//#endregion
+
+//#region "Start Connection"
+function StartConnection() {
+    connection.start().then(function () {
+        $('#btnMessage').prop('disabled', false);
+    }).catch(function (err) {
+        return console.error(err.toString());
+    });
+}
+//#endregion
+
+//#region "KeyDown"
+function SetKeyDown() {
+    $('#txtMessage').keydown(function (key) {
+        if (key.which == 13) {
+            $('#btnMessage').click();
+        }
+    })
+}
+//#endregion
